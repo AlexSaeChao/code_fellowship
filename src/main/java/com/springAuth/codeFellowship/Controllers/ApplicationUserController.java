838b359e63c25a33571db80e574b4c3b42eec472
AlexSaeChao <chaoalex93@gmail.com>
2023-08-01 21:28:52 -0700

    completed features display user info of logged in user, can view other's profiles as well

--- src/main/java/com/springAuth/codeFellowship/Controllers/ApplicationUserController.java
+++ src/main/java/com/springAuth/codeFellowship/Controllers/ApplicationUserController.java
@@ -9,10 +9,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Controller
@@ -44,9 +46,48 @@ public class ApplicationUserController {
             ApplicationUser applicationUser = applicationUserRepository.findByUsername(username);
 
             model.addAttribute("username", username);
+            model.addAttribute("FirstName", applicationUser.getFirstName());
+            model.addAttribute("LastName", applicationUser.getLastName());
         }
         return "index.html";
     }
+
+    @GetMapping("/test")
+    public String getTestPage(Principal p, Model m) {
+        if (p != null) {
+            String username = p.getName();
+            ApplicationUser applicationUser = applicationUserRepository.findByUsername(username);
+
+            m.addAttribute("Username", username);
+            m.addAttribute("Firstname", applicationUser.getFirstName());
+            m.addAttribute("LastName", applicationUser.getLastName());
+
+            System.out.println("Username: " + username);
+            System.out.println("FirstName: " + applicationUser.getFirstName());
+            System.out.println("LastName: " + applicationUser.getLastName());
+        }
+        return "test.html";
+    }
+
+    @GetMapping("/users/{id}")
+    public String getUserInfo(Model m, Principal p, @PathVariable Long id) {
+        if (p != null) {
+            String username = p.getName();
+            ApplicationUser applicationUser = applicationUserRepository.findByUsername(username);
+
+            m.addAttribute("BrowsingUserUsername", username);
+            m.addAttribute("BrowsingLastName", applicationUser.getLastName());
+            m.addAttribute("BrowsingFirstName", applicationUser.getFirstName());
+        }
+        ApplicationUser applicationUser = applicationUserRepository.findById(id).orElseThrow();
+        m.addAttribute("applicationUserUsername", applicationUser.getUsername());
+        m.addAttribute("applicationUserLastName", applicationUser.getLastName());
+
+        m.addAttribute("testDate", LocalDateTime.now());
+        
+        return "/user-info.html";
+    }
+
 
     @PostMapping("/signup")
     public RedirectView postSignup(String username, String password, String firstName, String lastName, Date dateOfBirth, String bio) {
